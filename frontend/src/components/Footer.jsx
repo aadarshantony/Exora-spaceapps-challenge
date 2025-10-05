5dc2e9eaf9ad503c309e10fe2ec22fbc012e21de
Adarsh <adarshantony223@gmail.com>
2025-10-05 12:15:42 +0530

    Final Updation

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <footer className="flex justify-center flex-col items-center p-6 text-sm text-[#8b949e] border-t border-[#30363d] mt-10 text-center">
+        <footer className="flex justify-center flex-col items-center p-6 text-sm text-[#8b949e] border-t border-[#30363d] mt-10 text-center w-full">
             <p>NASA Space Apps Challenge 2025 &bull; Powered by NASA Earth Observation Data</p>
             <p className='text-xs'>For better viewability, it's recommended to access the site from a desktop (laptop or computer).</p>
         </footer>
